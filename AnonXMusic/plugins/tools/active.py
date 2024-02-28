ba50097e22d5af980080eb2148ebdeb982f59edf
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-02-29 04:03:47 +0700

    Update active.py

--- AnonXMusic/plugins/tools/active.py
+++ AnonXMusic/plugins/tools/active.py
@@ -74,3 +74,10 @@ async def activevi_(_, message: Message):
             f"<b>» ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs :</b>\n\n{text}",
             disable_web_page_preview=True,
         )
+
+@app.on_message(filters.command("active", [".", "^", "-", "!", "/"]) & SUDOERS)
+async def activecilik(_, message: Message):
+    ms = len(await get_active_chats())
+    vd = len(await get_active_video_chats())
+    await app.send_message(message.chat.id, 
+        f"💽 Active Chats:**\n\n• **Music: {ms}\n•Video:{vd}")
