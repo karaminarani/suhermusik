96f5e6ffa3b1285be4380aace15ad388317f3ee1
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-04-17 15:29:46 +0700

    Update active.py

--- AnonXMusic/plugins/tools/active.py
+++ AnonXMusic/plugins/tools/active.py
@@ -39,7 +39,7 @@ async def activevc(_, message: Message):
         await mystic.edit_text(f"No Active Voice Chats On {app.mention}.")
     else:
         await mystic.edit_text(
-            f"<b>» List Of Currently Active Voice Chats :</b>\n\n{text}",
+            f"<b>List Of Currently Active Voice Chats :</b>\n\n{text}",
             disable_web_page_preview=True,
         )
 
